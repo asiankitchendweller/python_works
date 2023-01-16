eb31c634e396e0146411f9241887ccefd13e0b75
asiankitchendweller <fajaralqorni@gmail.com>
2023-01-16 14:32:00 +0700

    [UPDATE] The code is fixed.

--- grade_generator.py
+++ grade_generator.py
@@ -3,13 +3,13 @@ score = int(input("What score did you get? "))
 percentage = score // 100
 if score < 50:
 	print(f"""Your score is {percentage}%, and you got an U for that. Sorry, I think this field is just not for you :(""")
-elif score > 50:
+elif score > 50 and score < 60:
 	print(f"Your score is {percentage}%, and you got a D for that. Please study harder, I know you got this.")
-elif score >= 60:
+elif score >= 60 and score < 70:
 	print(f"Your score is {percentage}%, and you got a C! Not too shabby. Let's see what your Asian parents think about it.")
-elif score >= 70:
+elif score >= 70 and score < 80:
 	print(f"B is really not a bad score, with the percentage of {percentage}. You're just almost there!")
-elif score >= 80:
-	print(f"Woo! YOu got a pretty good score out there, pal! {percentage}% is very impressive! I hope your parents are proud and not comparing you to that nerd next door.")
-elif score >= 90 or score <= 100:
-	print("A++")
+elif score >= 80 and score < 90:
+	print(f"Woo! YOu got a pretty good score out there, pal! {percentage}% is very impressive! I hope your parents are proud and not comparing you to that nerd next door. 🤓")
+elif score >= 90 and score <= 100:
+	print(f"Astounding! A++ for a change! Your friends are going to envy you, and your existence is in danger if you're from some extreme states of the world. Worry not! You're already this smart to finish this code so you'd have to know how to hide yourself from CIA agents. 👮‍♂️👮‍♂️")
